be0a1a5701e4990070544be1495a5fd326614b69
Juan Pablo <juanpablofc8.0@gmail.com>
2026-06-28 02:27:07 -0300

    feat: criacao do app tasks, model da tabela e migrations iniciais

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    creat_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = "task"
+        managed = True
+    
+    def __str__(self):
+        return self.title
