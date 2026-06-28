d2a79f1ff90f92e0b227569bfb6fc3af90ff4304
Juan Pablo <juanpablofc8.0@gmail.com>
2026-06-28 02:39:25 -0300

    feat: criacao do TaskSerializer no app tasks

---
+++ tasks/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = fields = [
+            'id', 
+            'title', 
+            'description', 
+            'is_completed', 
+            'created_at', 
+            'category'
+        ]
