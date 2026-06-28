8da6cbda78cc2a0716989cb9e0f57356acaa0cfb
Juan Pablo <juanpablofc8.0@gmail.com>
2026-06-28 06:20:05 -0300

    chore: modifica posição da variável category para corresponder a posição da coluna na tabela do banco de dados

--- tasks/models.py
+++ tasks/models.py
@@ -5,8 +5,8 @@ class Task(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
     is_completed = models.BooleanField(default=False)
+    category = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
-    category = models.CharField(max_length=10)
 
     class Meta:
         db_table = "task"
@@ -14,3 +14,7 @@ class Task(models.Model):
     
     def __str__(self):
         return self.title
+    
+
+
+    
